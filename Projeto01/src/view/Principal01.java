a92b550113d7d09f81e5e3104e24638611db9c31
Rodrigo Santos <rodrigo.santos5548@gmail.com>
2025-09-03 00:03:42 -0300

    Commit_Test01

---
+++ Projeto01/src/view/Principal01.java
@@ -0,0 +1,17 @@
+
+package view;
+
+import java.util.*;
+
+public class Principal01 {
+
+	public static void main(String[] args) {
+		
+		Scanner ls = new Scanner(System.in);
+		int x = ls.nextInt();
+		System.out.print(x);
+		ls.close();
+
+	}
+
+}
